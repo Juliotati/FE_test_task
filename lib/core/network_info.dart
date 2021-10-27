2e21e1544965b6c728135e1e38120f62be11933d
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-27 13:48:24 +0100

    feat(core connectivity): added network info class

---
+++ lib/core/network_info.dart
@@ -0,0 +1,45 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetWorkInfoImpl implements NetworkInfo {
+  NetWorkInfoImpl._();
+
+  static NetWorkInfoImpl instance = NetWorkInfoImpl._();
+
+  @override
+  Future<bool> get isConnected => _isConnected();
+
+  Future<void> noConnectionWarning(BuildContext context) async {
+    if (await _hasNoNetwork()) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Oops you seem to be offline'),
+        ),
+      );
+      return;
+    }
+  }
+
+  Future<bool> _isConnected() async {
+    if (await _hasNoNetwork()) return false;
+    if (await _hasNetwork()) return true;
+    return false;
+  }
+
+  Future<bool> _hasNetwork() async {
+    final ConnectivityResult result = await Connectivity().checkConnectivity();
+    return result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.wifi;
+  }
+
+  Future<bool> _hasNoNetwork() async {
+    final ConnectivityResult result = await Connectivity().checkConnectivity();
+    return result == ConnectivityResult.none || !(await _hasNetwork());
+  }
+}
