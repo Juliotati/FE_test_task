c8fd549ce3b97535364c30eabaef81b2e34e53ea
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-10-27 15:16:28 +0100

    config(providers): register network info provider, hide debug bunner

--- lib/fe_main.dart
+++ lib/fe_main.dart
@@ -1,21 +1,36 @@
+import 'package:fe_test_task/core/network_info.dart';
+import 'package:fe_test_task/features/posts/data/datasources/remote_datasource.dart';
 import 'package:fe_test_task/features/posts/posts.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 class FETestApp extends StatelessWidget {
   const FETestApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: <String, Widget Function(BuildContext)>{
-        Posts.route: (BuildContext context) => const Posts(),
-        PostDetails.route: (BuildContext context) => const PostDetails(),
-      },
-      title: 'FE Posts',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
+    return MultiProvider(
+      providers: <SingleChildWidget>[
+        ListenableProvider<PostsNetwork>(create: (BuildContext context) {
+          return PostsNetwork();
+        }),
+        Provider<NetWorkInfoImpl>(create: (BuildContext context) {
+          return NetWorkInfoImpl.instance;
+        }),
+      ],
+      child: MaterialApp(
+        title: 'FE Posts',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const Posts(),
+        routes: <String, Widget Function(BuildContext)>{
+          Posts.route: (BuildContext context) => const Posts(),
+          PostDetails.route: (BuildContext context) => const PostDetails(),
+        },
       ),
-      home: const Posts(),
     );
   }
 }
